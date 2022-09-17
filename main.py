b2a9ab16eed5a4312667f2d0f724aa6987dc22d4
Santiago <santiagoruiz68@yahoo.com>
2022-09-17 16:24:35 -0600

    built login page

--- main.py
+++ main.py
@@ -2,14 +2,31 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import DateTime
 from sqlalchemy.sql import text, func
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 
 db_name = 'racketinfo.db'
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # this variable, db, will be used for all SQLAlchemy commands
 db = SQLAlchemy(app)
@@ -31,13 +48,46 @@ class RacketForm(db.Model):
     def __repr__(self):
         return '<RacketForm %r>' % self.player_name
 
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
 @app.route("/")
+@login_required
 def home():
     return render_template('home.html')
 
     #Lines 38–53 provide a model so that Python can translate the racket info table.
 
 @app.route("/racket", methods= ["GET", "POST"])
+@login_required
 def rackets():
     if request.method == "POST":
         racket_form_response = RacketForm(
@@ -63,10 +113,12 @@ def rackets():
 
 
 @app.route("/racket/new", methods= ["GET"])
+@login_required
 def createRacket():
     return render_template('form.html')
 
 @app.route("/racket/<int:racket_id>", methods= ["POST"])
+@login_required
 def update(racket_id):
     if request.method == "POST":
         racket_request = RacketForm.query.filter_by(id=racket_id).first()
@@ -75,6 +127,35 @@ def update(racket_id):
         db.session.commit()
         return redirect(url_for('rackets'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('rackets'))
+    return render_template('login.html', form=form)
+
+@ app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 # NOTHING BELOW THIS LINE NEEDS TO CHANGE
 # this route will test the database connection and nothing more
